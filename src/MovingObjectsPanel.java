ba40392bbabb22824e40d4cfab0edef388192092
Brandon Phan <Brandon.phan.73@gmail.com>
2017-02-10 11:31:27 -0800

    Update MovingObjectsPanel.java

--- src/MovingObjectsPanel.java
+++ src/MovingObjectsPanel.java
@@ -1,88 +1,165 @@
 import java.awt.Dimension;
-import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.AbstractAction;
 import javax.swing.JPanel;
 import javax.swing.KeyStroke;
+import java.awt.event.KeyEvent;
 import javax.swing.Timer;
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
-
-public class MovingObjectsPanel extends JPanel {
+public class MovingObjectsPanel extends JPanel implements MouseMotionListener {
 	
 	final Dimension defaultDim;// = new Dimension(800,600);
-	DiepIOMap gm;
-	
-	private Timer timer;
-	
+	private DiepIOMap gm;
+	private Timer t;
+
 	public MovingObjectsPanel() {
 		this( new Dimension(800,600));
 	}
 	public MovingObjectsPanel(Dimension dim) {
 		defaultDim = dim;
+		gm = new DiepIOMap();
 		this.setPreferredSize(defaultDim);
 		makeGameMap();
-		setupTimer();
+		t.start();
+		addMouseMotionListener(this);
 		setUpKeyMappings();
+		setUpClickListener();
 	}
-	
-	private void setupTimer() {
-		timer = new Timer(100, new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				tick();
-				repaint();
-			}
-		});
-		timer.start();
-	}
-	
 	private void makeGameMap() {
 		gm = new DiepIOMap();
+		t = new Timer(10, new ActionListener() {// fires off every 10 ms
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				gm.tick();// I tell the GameMap to tick... do what
+					// you do every time the clock goes off.
+				repaint();// naturally, we want to see the new view
+			}	
+		});
 	}
+
+	private void setUpClickListener() {
+		this.requestFocusInWindow();
+		this.addMouseListener(new MouseListener() {
+
+			//	If you want to detect mouse dragging, then use a mouseMotionListener
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+
+			}
+			@Override
+			public void mouseExited(MouseEvent arg0) {
+
+			}
+
+			@Override
+			public void mousePressed(MouseEvent click) {
+				clickedAt(click);
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent arg0) {
+				clickedAt(arg0);
+			}
+
+		});
+	}
+
+	protected void clickedAt(MouseEvent click) {
+		gm.playerShoot();
+		repaint();
+	}
+
+  	public void mouseMoved(MouseEvent e) {
+       gm.mouseMoved(e);
+       repaint();
+    }
+
+    public void mouseDragged(MouseEvent e) {
+    }
 	
 	private void setUpKeyMappings() {
-		// maps keys with actions...
-		//  The code below maps a KeyStroke to an action to be performed
-		// In this case I mapped the space bar key to the action named "shoot"
-		// Whenever someone hits the Space Bar the action shoot is sent out
 
-		this.getInputMap().put(KeyStroke.getKeyStroke("SPACE"),"shoot");
+		this.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_W , 0, false),"forward");
+		// this.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_W, 0, true),"stopForward");
 
-		//  This associates the command shoot with some action.  In this 
-		// case, the action triggers a shoot command invoked on my GameMap.  In general, whatever 
-		// goes in the actionPerformed method will be executed when a shoot command
-		// is sent...
-			
-		this.getActionMap().put("shoot",new AbstractAction(){
+		this.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_A, 0, false),"left");
+		// this.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_A, 0, true),"stopLeft");
+
+		this.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_S, 0, false),"reverse");
+		// this.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_S, 0, true),"stopReverse");
+
+		this.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_D, 0, false),"right");
+		// this.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_D, 0, true),"stopRight");
+
+		this.getActionMap().put("forward",new AbstractAction(){
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				gm.playerShoot();
-				for (int i = 0; i < gm.objects.size(); i++) {
-					GameObject go = gm.objects.get(i);
-					System.out.println(i+"- speed="+go.speed+", location="+go.location);
-					if (go instanceof Bullet) {
-						System.out.println("is a bullet");
-					}
-					if (go instanceof Tank) {
-						System.out.println("is a tank");
-					}
-				}
-				System.out.println("shooting");
+				gm.playerForward();
+			}
+		});	
+		// this.getActionMap().put("stopForward",new AbstractAction(){
+		// 	@Override
+		// 	public void actionPerformed(ActionEvent e) {
+		// 		gm.playerForward();
+		// 	}
+		// });		
+
+		this.getActionMap().put("left",new AbstractAction(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				gm.playerLeft();
+			}
+		});		
+		// this.getActionMap().put("stopLeft",new AbstractAction(){
+		// 	@Override
+		// 	public void actionPerformed(ActionEvent e) {
+		// 		gm.playerLeft();
+		// 	}
+		// });	
+
+		this.getActionMap().put("reverse",new AbstractAction(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				gm.playerReverse();
+			}
+		});		
+		// this.getActionMap().put("stopReverse",new AbstractAction(){
+		// 	@Override
+		// 	public void actionPerformed(ActionEvent e) {
+		// 		gm.playerReverse();
+		// 	}
+		// });	
+
+		this.getActionMap().put("right",new AbstractAction(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				gm.playerRight();
 			}
 		});
+		// this.getActionMap().put("stopRight",new AbstractAction(){
+		// 	@Override
+		// 	public void actionPerformed(ActionEvent e) {
+		// 		gm.playerRight();
+		// 	}
+		// });
+
 		this.requestFocusInWindow();		
 	}
-	
-	public void tick() {
-		gm.tick();
-	}
-	
+
 	@Override
-	protected void paintComponent(Graphics g) {
+	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		gm.draw(g);
 	}
-	
+
 }
