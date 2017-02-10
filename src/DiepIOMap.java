f052af300b342882a6b021b42875cdd8d2a3462a
Brandon Phan <Brandon.phan.73@gmail.com>
2017-02-10 11:31:47 -0800

    Update DiepIOMap.java

--- src/DiepIOMap.java
+++ src/DiepIOMap.java
@@ -1,10 +1,10 @@
-import java.awt.Graphics;
 import java.awt.Image;
 import java.util.ArrayList;
 import java.util.List;
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
 
-
-public class DiepIOMap{
+public class DiepIOMap {
 
 	public List<GameObject> objects;
 	private Tank playerTank;
@@ -29,21 +29,72 @@ public class DiepIOMap{
 	public void openBackgroundImage() {
 
 	}
-	
-	public void playerShoot() {
-		addGameObject(playerTank.shoot());
-	}
-	
-	public void draw(Graphics g) {
-		for (GameObject go : objects) {
-			go.draw(g);
-		}
+
+	int xStart, yStart, xEnd, yEnd;
+	public void mouseMoved(MouseEvent e) {
+		xEnd = e.getX();
+		yEnd = e.getY();
+
+		xStart = (int) (playerTank.location().getX());
+		yStart = (int) (playerTank.location().getY());
+
+		double hypotenuseLength = Math.sqrt(((Math.pow(xEnd - xStart, 2)) + (Math.pow(yEnd - yStart, 2))));
+		double verticalLength = Math.sqrt((Math.pow(xEnd - xEnd, 2)) + (Math.pow(yEnd - yStart, 2)));
+
+		double direction = Math.asin(verticalLength / hypotenuseLength);
+
+		System.out.println("Hypotenuse: " + hypotenuseLength + " verticalLength: " + verticalLength + " Angle: " + direction);
+		//	Hypotenuse
+		// g.drawLine((int)xStart, (int)yStart, (int)xEnd, (int)yEnd);
+
+		// Horizontal line
+		// g.drawLine(xStart, yStart, xEnd, yStart);
+
+		//	Vertical line
+
+		// g.drawLine(xEnd, yStart, xEnd, yEnd);
+		playerTank.aimWeapon(direction);
 	}
 
 	public void tick() {
 		for (GameObject go : objects) {
 			go.move();
 		}
+	}
+	
+	public void playerShoot() {
+		addGameObject(playerTank.shoot());
+	}
+
+	public void playerLeft() {
+		playerTank.left();
+	}
+
+	public void playerRight() {
+		playerTank.right();
+	}
+
+	public void playerForward() {
+		playerTank.forward();
+	}
+
+	public void playerReverse() {
+		playerTank.reverse();
+	}
+
+	public void draw(Graphics g) {
+		for (GameObject go : objects) {
+			go.draw(g);
+		}
+		//	Hypotenuse
+		g.drawLine((int)xStart, (int)yStart, (int)xEnd, (int)yEnd);
+
+		//	Horizontal line
+		g.drawLine(xStart, yStart, xEnd, yStart);
+
+		//	Vertical line
+		g.drawLine(xEnd, yStart, xEnd, yEnd);
+
 	}
 
 }
