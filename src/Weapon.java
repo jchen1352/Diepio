c968ef197d0bcc196e196d7de42484110b274237
Brandon Phan <Brandon.phan.73@gmail.com>
2017-02-10 11:32:24 -0800

    Update Weapon.java

--- src/Weapon.java
+++ src/Weapon.java
@@ -10,12 +10,15 @@ public class Weapon {
 	 * Shoots a bullet from the given location in the given direction
 	 * with the given speed.
 	 * @param location the location to spawn the bullet
-	 * @param direction the direction the bullet will travel
 	 * @param speed how fast the bullet will travel
 	 * @return the bullet that was created
 	 */
-	public Bullet shoot(Location location, double direction, double speed) {
-		return new Bullet(location, direction, speed, 5, 5);
+	public Bullet shoot(Location location, double speed) {
+		return new Bullet(location, this.direction, speed, 5, 5);
+	}
+
+	public void aim(double direction) {
+		this.direction = direction;
 	}
 	
 }
