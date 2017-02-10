34ec4211c1a94370646c2a4f680026fcabe78e5f
Brandon Phan <Brandon.phan.73@gmail.com>
2017-02-10 11:32:04 -0800

    Update Tank.java

--- src/Tank.java
+++ src/Tank.java
@@ -1,4 +1,5 @@
 import java.awt.Graphics;
+import java.awt.Rectangle;
 
 
 public class Tank extends GameObject {
@@ -9,17 +10,47 @@ public class Tank extends GameObject {
 		super(location, width, height);
 		weapon = new Weapon(direction);
 	}
-	
-	public Bullet shoot(){
-		return weapon.shoot(new Location(location), direction, 5);
+
+	public Bullet shoot() {
+		return weapon.shoot(new Location(location), 5);		
+	}
+
+	public void forward() {
+		speed = 1;
+		direction = -1.57;
+		this.move();
+	}
+
+	public void left() {
+		speed = 1;
+		direction = 3.14;
+		this.move();
+	}
+
+	public void reverse() {
+		speed = 1;
+		direction = 1.57;
+		this.move();
 	}
 	
-	
+	public void right() {
+		speed = 1;
+		direction = 0;
+		this.move();
+	}
+
+	public void aimWeapon(double direction) {
+		weapon.aim(direction);
+	}
+
+	public Location location() {
+		return location;
+	}
+
 	@Override
 	public void checkOffScreen() {
-
+		// TODO Auto-generated method stub
 	}
-
 
 	@Override
 	public void draw(Graphics g) {
