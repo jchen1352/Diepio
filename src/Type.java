6500d6aa72b3a8aa04ba35681f75c44fd5857ced
Brandon <Brandon.phan.73@gmail.com>
2017-02-17 11:05:49 -0800

    Added type.java

---
+++ src/Type.java
@@ -0,0 +1,14 @@
+public enum Type {
+	SQUARE(20,20, 4),
+	TRIANGLE(20,20, 3),
+	PENTAGON(15,15, 5);
+		
+	public final double width;
+	public final double height;
+	public final int numSides;
+	private Type(double width, double height, int numSides) {
+		this.width = width;
+		this.height = height;
+		this.numSides = numSides;
+	}
+};
